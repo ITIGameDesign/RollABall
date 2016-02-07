bf4435b431a2ded9990bbae9f382f1083e64732f
Folaranmi Ogunfemi <fola.ogunfemi@gmail.com>
2016-02-07 10:47:54 -0500

    GitHub Conversion
    Upload to Professional Github

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Controller : MonoBehaviour 
+{
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+
+	void Start ()
+	{
+				count = 0;
+				SetCountText ();
+				winText.text = "";
+		}
+
+	void FixedUpdate ()
+	{
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+					rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Pickup")
+		{
+						other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString (); 
+		if (count >= 12) 
+		{
+						winText.text = "You Win!";
+		}
+	}
+}
